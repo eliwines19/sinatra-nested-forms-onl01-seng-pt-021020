77dac3c49843f1f48abb1e554cf19cc1ec84858a
Elijah wines <elijahwines84@yahoo.com>
2020-05-07 15:30:34 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = oarams[:height]
+    @@all << self 
+  end
+
+  def self.all 
+    @@all
+  end
+  
 end
