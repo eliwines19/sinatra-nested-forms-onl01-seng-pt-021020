63a9275152201dd733cd61e158321a3dacbe1a2f
Elijah wines <elijahwines84@yahoo.com>
2020-05-07 16:01:12 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,17 +1,16 @@
 class Pirate
-  attr_accessor :name, :weight, :height
+  attr_reader :name, :weight, :height
 
   @@all = []
 
   def initialize(params)
-    @name = params[:name]
-    @weight = params[:weight]
-    @height = oarams[:height]
-    @@all << self
-  end
+      @name = params[:name]
+      @weight = params[:weight]
+      @height = params[:height]
+      @@all << self
+  end 
 
   def self.all
-    @@all
+      @@all
   end
-
 end
